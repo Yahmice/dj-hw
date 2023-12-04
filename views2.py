1632fbd60f416ec300069fce56c1d27d99fe7497
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-12-04 17:38:23 +0500

    Update views2.py

--- views2.py
+++ views2.py
@@ -1,50 +1,15 @@
-from django.shortcuts import render
+def cook(request, recipe_name):
+    if recipe_name in DATA.keys():
+        servings = request.GET.get('servings', 1)
+        data = {}
+        for ingredient in DATA[recipe_name]:
+            data[ingredient] = round(DATA[recipe_name][ingredient] * int(servings), 2)
 
-DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
-    },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
-    },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-}
+        context = {
+            'recipe': data
+        }
 
-def omlet(request):
-    for dishes, ingredients in DATA.items():
-        if dishes == 'omlet':
-            ingredient = ingredients
-    servings = request.GET['servings']
-    context = {
-        'data': ingredient,
-        'servings': servings
-    }
-    return render(request, 'calculator/index.html', context)
+    else:
+        context = {}
 
-
-def pasta(request):
-    for dishes, ingredients in DATA.items():
-        if dishes == 'pasta':
-            ingredient = ingredients
-    context = {
-        'data': ingredient
-    }
-    return render(request, 'calculator/index.html', context)
-
-
-def buter(request):
-    for dishes, ingredients in DATA.items():
-        if dishes == 'buter':
-            ingredient = ingredients
-    context = {
-        'data': ingredient
-    }
     return render(request, 'calculator/index.html', context)
